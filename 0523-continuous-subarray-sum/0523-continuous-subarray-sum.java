dbf338004ae43d3022d316a42429cc36c11b4d76
Arjun Kumar Pandey <93878505+arjunkumarpandey@users.noreply.github.com>
2024-06-09 19:12:16 +0530

    Time: 21 ms (88.12%), Space: 57.1 MB (93.33%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,26 +1,21 @@
-import java.util.HashMap;
-
 class Solution {
-
     public boolean checkSubarraySum(int[] nums, int k) {
-        int prefixMod = 0;
-        HashMap<Integer, Integer> modSeen = new HashMap<>();
-        modSeen.put(0, -1);
+        int n=nums.length;
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        mp.put(0,-1);
+        int mod=0;
 
-        for (int i = 0; i < nums.length; i++) {
-            prefixMod = (prefixMod + nums[i]) % k;
-
-            if (modSeen.containsKey(prefixMod)) {
-                // ensures that the size of subarray is at least 2
-                if (i - modSeen.get(prefixMod) > 1) {
+        for(int i=0;i<n;i++){
+            mod=(mod+nums[i]) % k;
+            if(mp.containsKey(mod)){
+                int size=i-mp.get(mod);
+                if(size>=2){
                     return true;
                 }
-            } else {
-                // mark the value of prefixMod with the current index.
-                modSeen.put(prefixMod, i);
+            }else{
+                mp.put(mod,i);
             }
         }
-
         return false;
     }
 }
